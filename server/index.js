a83bdae1180ff190d52cb4451a45a7658c46fa5b
Cody Gentz <gentzcody@gmail.com>
2022-10-13 14:25:14 -0500

    [BEETS-3] Adding packages to API and fixing NPM command

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 5000
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`BEETS-API listening on port ${PORT}`);
 })
