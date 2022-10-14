eabb14d824ff73ce311c9ad1562da3b11fd44023
PirateCody <gentzcody@gmail.com>
2022-10-13 22:49:17 -0500

    [BEETS-3] Adding additional packages and file structure changes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
