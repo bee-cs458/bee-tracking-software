abd3b69d21c7f0be893b7d6a49421c0f95b6ef40
Cody Gentz <gentzcody@gmail.com>
2022-10-17 12:55:03 -0500

    [BEETS-3] Updating environment variables

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.API_PORT || process.env.PORT || 5000;
 
 app.use("/api/count", counterRoutes);
 
