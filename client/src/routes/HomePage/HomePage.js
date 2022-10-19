529240dc9844879f394ad77578b2837c8f234e57
prewe5294 <prewe5294@my.uwstout.edu>
2022-10-19 14:22:34 -0500

    made navbar and header w react, change->bootstrap

--- client/src/routes/HomePage/HomePage.js
+++ client/src/routes/HomePage/HomePage.js
@@ -1,23 +1,32 @@
-import logo from '../../assets/logo.svg';
+import { Link } from "react-router-dom";
 import './HomePage.css';
+
+import list from '../../assets/list.png';
+import search from '../../assets/search.png'
 
 function HomePage() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <div className="header-container container-fluid">
+                <div className="menu-header">
+                    <Link to="/">
+                        <img src={list} alt="list" width="30" height="30"/>
+                    </Link>
+                </div>
+                <div className="search-header">
+                    <form action="/" />
+                        <input type="text" class="form-control" id="search" placeholder="Search" name="search"/>
+
+                    <button type="submit" class="btn btn-default">
+                    <img src={search} alt="search" width="22" height="22"/>
+                    </button>
+                </div>
+                <div className="right-header"></div>
+            </div>
+            <div className="container-fluid main-content">
+                <p>hello hello hello hello hello hello hello</p>
+
+            </div>
         </div>
     );
 }
