0f05b7b52cf98bf428f5c599689766d1ee9164d5
PirateCody <gentzcody@gmail.com>
2022-10-18 23:22:33 -0500

    [BEETS-5] Added demonstration database call

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { query } from '../utilities/DatabaseUtilities.js'
 const router = express.Router();
 
 // import all api routes here
@@ -7,6 +8,14 @@ import counterRoutes from './CounterRoutes.js'
 // our api routes
 router.get("/", (req, res) => res.send("Hello World!"));
 router.use("/count", counterRoutes);
+
+// Test Database Endpoint - demonstration purposes only
+router.get("/dbTest", async (req, res) => {
+
+    const result = await query("SELECT * FROM beets_staging.asset");
+    res.send(result);
+
+});
 
 // catch-all for /api/something-not-valid
 router.use("/", (req, res) => res.status(404).send(`API route ${req.url} is not defined`));
