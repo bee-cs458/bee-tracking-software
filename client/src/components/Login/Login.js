45820e40c99f9bd37eb424a37fd2d3d3c89830c3
ColbyTief <colbytief123@gmail.com>
2022-10-24 14:04:05 -0500

    create login.js

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,18 @@
+import Popup from 'react-popup';
+
+
+
+function login() {
+
+return(
+    <div>
+        <input type="text"></input>
+
+        <input type="text"></input>
+
+    </div>
+);
+
+}
+
+export default login;
