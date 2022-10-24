73ca3a16b5c03933a2d45b782038ef9c644d9a1e
Ella Prew <prewe5294@my.uwstout.edu>
2022-10-24 13:16:51 -0500

    Added sign in button to header and fixed screen sizing
    Forgot sign in button so added that, also the screen was larger than it needed to be, making the scroll bars show up so I fixed that

--- client/src/routes/HomePage/HomePage.js
+++ client/src/routes/HomePage/HomePage.js
@@ -2,18 +2,19 @@ import { Link } from "react-router-dom";
 import './HomePage.css';
 
 import list from '../../assets/list.png';
-import search from '../../assets/search.png'
+import search from '../../assets/search.png';
+import signIn from '../../assets/signIn.png';
 
 function HomePage() {
     return (
         <div className="App">
-            <div className="header-container container-fluid">
+            <div className="header-container">
                 <div className="menu-header">
                     {/* Not sure the purpose of this, it was on cheqroom, might end up 
-                            being a dropdown menu instead of a link */}
-                    <Link to="/">
+                            being a dropdown menu instead of a button */}
+                    <button type="menu" class="btn btn-default">
                         <img src={list} alt="list" width="30" height="30"/>
-                    </Link>
+                    </button>
                 </div>
                 <div className="search-header">
                     {/* form waiting to be linked */}
@@ -24,11 +25,15 @@ function HomePage() {
                     </button>
                 </div>
                 {/* Extra space next to search bar to fill header, intended to stay empty */}
-                <div className="right-header"></div>    
+                <div className="right-header">
+                    <form action="/" />
+                        <button type="signIn" class="btn btn-default">Sign In
+                        <img src={signIn} alt="signIn" width="22" height="22"/></button>
+                        
+                </div>    
             </div>
-            <div className="container-fluid main-content">
+            <div className="main-content">
                 {/* Container for the main section of the page, can be edited to include more functions */}
-                
 
             </div>
         </div>
