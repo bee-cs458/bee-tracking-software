bb7d2aa6641118512880d344e1e7b0f330b922ed
Cody Gentz <gentzcody@gmail.com>
2022-11-09 14:25:08 -0600

    BEETS-11 start of the basic logic

--- controllers/CheckoutController.js
+++ controllers/CheckoutController.js
@@ -1,7 +1,7 @@
 import { query, insert_params, insert_values, update_params, where_params_like } from "../utilities/DatabaseUtilities.js";
 
 export const getAllCheckoutRecords = async (req, res, next) => {
-    await query(``
+    await query(`SELECT * FROM checkoutrecords`
     ).then(
         (result) => { res.send({ result }) }, // on success
         (reason) => {
@@ -11,12 +11,31 @@ export const getAllCheckoutRecords = async (req, res, next) => {
     );
 }
 
+export const getCheckoutStatus = async (req, res, next) => {
+
+    //const asset_tag = req.params.id;
+
+    //var asset = await query(`SELECT * FROM`);
+
+}
+
 export const checkoutAsset = async (req, res, next) => {
 
     // check that the asset(s) exist
+        // select the individual assets from the DB and check if they are null
+    for(var tag in req.asset_tags){
+        var exists = query(`SELECT 1 FROM assets WHERE asset_tag=${tag}`);
+        if(exists == null){
+
+            res.status(404);
+
+        }
+    }
         // if not, 404
     // check if the asset(s) are already checked out
-        // if so, 409
+        // get the latest checkout record(s) associated with the asset_tag(s)
+            // if the in_date(s) are null, then they are checked out
+            // if so, 409
     // check whether the student has < 3 strikes
         // if not, 401
     // if all above pass, create checkout record(s)
--- routes/CheckoutRoutes.js
+++ routes/CheckoutRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 
-import { getAllCheckoutRecords, checkoutAsset } from '../controllers/CheckoutController.js';
+import { getAllCheckoutRecords, getCheckoutStatus, checkoutAsset } from '../controllers/CheckoutController.js';
 import { requireBody, restrictTo, requireQuery, filterQuery } from '../controllers/SecurityController.js'
 
 
 const router = express.Router();
 
 router.get("/all_records", getAllCheckoutRecords);
+router.get("/status/:id", getCheckoutStatus)
 router.post("/checkout_assets", requireBody("asset_tags", "user_id", "due_date"), checkoutAsset);
 
 export default router;
