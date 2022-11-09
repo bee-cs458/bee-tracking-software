0ef1790afc7a8e0f28272ebabade9d88b2cea72e
Cody Gentz <gentzcody@gmail.com>
2022-11-09 13:26:02 -0600

    BEETS-11 export router

--- routes/CheckoutRoutes.js
+++ routes/CheckoutRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getAllCheckoutRecords, checkoutAsset } from '../controllers/CheckoutController';
+import { getAllCheckoutRecords, checkoutAsset } from '../controllers/CheckoutController.js';
 import { requireBody, restrictTo, requireQuery, filterQuery } from '../controllers/SecurityController.js'
 
 
@@ -8,3 +8,5 @@ const router = express.Router();
 
 router.get("/all_records", getAllCheckoutRecords);
 router.post("/checkout_assets", requireBody("asset_tags", "user_id", "due_date"), checkoutAsset);
+
+export default router;
