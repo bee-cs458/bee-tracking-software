7596658a7ed97bcc7629b79ba78f090cc307b3d5
ColbyTief <colbytief123@gmail.com>
2022-11-14 13:55:27 -0600

    UserRoute.js copied

---
+++ routes/UserRoute.js
@@ -0,0 +1,7 @@
+import {
+    updateUser
+} from "../controllers/UserController";
+
+import { requireBody, restrictTo, requireQuery, filterQuery } from '../controllers/SecurityController.js'
+
+router.post("/:id", restrictTo('operator'), requireBody(), updateUser);
