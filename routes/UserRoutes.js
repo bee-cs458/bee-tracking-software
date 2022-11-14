a6099a200466519695f7b6c8633e216446e4fac4
ColbyTief <colbytief123@gmail.com>
2022-11-14 15:23:00 -0600

    updated route

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -4,6 +4,6 @@ import {
 import { requireBody, restrictTo, requireQuery, filterQuery } from '../controllers/SecurityController.js'
 import router from "./AssetRoutes";
 
-router.post("/:id", restrictTo('operator'), requireBody('username', 'password', 'newPassword'), updateUser);
+router.post("/update_password", restrictTo('operator'), requireBody('username', 'password', 'newPassword'), updateUser);
 
 export default router;
