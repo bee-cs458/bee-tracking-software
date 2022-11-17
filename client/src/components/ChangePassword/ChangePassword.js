9c9ce9d8365fb912563426283cb91aea19804b3e
ColbyTief <colbytief123@gmail.com>
2022-11-17 13:13:51 -0600

    Change password condition updated
    request to change password will no longer be sent if the user does not verify their new password, also slightly changed some visual elements.

--- client/src/components/ChangePassword/ChangePassword.js
+++ client/src/components/ChangePassword/ChangePassword.js
@@ -1,24 +1,32 @@
 import Button from "react-bootstrap/esm/Button";
 import {updatePass, updatePassword } from '../../api/UserService.js';
+import './ChangePassword.css';
+
+function verifyPass(nPass1, nPass2) {
+return((nPass1 == nPass2))
+}
 
 function ChangePassword() {
     return(
         <div style={{ position: 'absolute',
-                        left: '400px',
-                        top: '100px' }}>
-                <p>Current Password:</p><br></br>
+                        left: '300px',
+                        top: '40px' }}>
+                <p>Current Password:</p>
                 <input id="passwordCur" type="password"></input>
-                <br></br>
-                <p>New Password:</p><br></br>
+                
+                <p>New Password:</p>
                 <input id="passwordNew" type="password"></input>
-                <br></br>
-                <p>Verify Password:</p><br></br>
+                
+                <p>Verify Password:</p>
                 <input id="passwordNew2" type="password"></input>
+                <br/>
                 <div>
                 <Button variant="primary" onClick={
                      async () => {
-                        updatePassword(document.getElementById('passwordCur').value,document.getElementById('passwordNew').value);
-                        }} >  Update Password </Button>
+                       (verifyPass(document.getElementById('passwordNew').value, document.getElementById('passwordNew2').value) ? 
+                        updatePassword(document.getElementById('passwordCur').value, document.getElementById('passwordNew').value): console.log("Passwords do not match") );
+                        }} style={{ position: 'relative',
+                                    top: '10px' }} >  Update Password </Button>
               
                 </div>
             
