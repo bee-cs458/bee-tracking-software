106d5f998a20ab40415e18d9045c89b2fccfa6b9
levertyk <levertyk4517@my.uwstout.edu>
2022-11-30 14:08:28 -0600

    beets-185 Change Password Formatting

--- client/src/routes/ProfilePage/ProfilePage.js
+++ client/src/routes/ProfilePage/ProfilePage.js
@@ -1,14 +1,14 @@
 import ChangePassword from "../../components/ChangePassword/ChangePassword";
+import "./ProfilePage.css";
 
-
-export default function ProfilePage(){
-
-    return(<>
-        <div className="header-container"></div>
-        
+export default function ProfilePage() {
+  return (
+    <div>
+      <div className="header-container"></div>
+      <div className="main-content">
+        <h1>Change Password</h1>
         <ChangePassword></ChangePassword>
-        
-        
-        
-        </>);
-};
+      </div>
+    </div>
+  );
+}
