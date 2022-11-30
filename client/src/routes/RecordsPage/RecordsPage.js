6e23211699ea4444ffbfd704d80e2525a663b18d
ColbyTief <colbytief123@gmail.com>
2022-11-30 15:22:59 -0600

    Beets-27 Records page setup

---
+++ client/src/routes/RecordsPage/RecordsPage.js
@@ -0,0 +1,13 @@
+import RecordTable from "../../components/RecordsUtils/RecordTable.js";
+
+export default function RecordsPage(){
+
+    return(<>
+        <div className="header-container"></div>
+        
+        
+        <RecordTable></RecordTable>
+        
+        
+        </>);
+};
