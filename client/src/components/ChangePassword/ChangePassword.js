b51b48a4feec6d3f15934738ea10ebd616121e64
levertyk <levertyk4517@my.uwstout.edu>
2022-11-30 13:43:59 -0600

    beets-185 Initial Setup
    Included password strength package for checking password. Changed password values to hooks.

--- client/src/components/ChangePassword/ChangePassword.js
+++ client/src/components/ChangePassword/ChangePassword.js
@@ -1,53 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import {updatePass, updatePassword } from '../../api/UserService.js';
-import './ChangePassword.css';
+import { updatePass, updatePassword } from "../../api/UserService.js";
+import "./ChangePassword.css";
+import { passwordStrength } from 'check-password-strength'
 
 function verifyPass(nPass1, nPass2) {
-return((nPass1 == nPass2))
+  return nPass1 == nPass2;
 }
 
-function password(){
+export default function ChangePassword() {
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [passwordAgain, setPasswordAgain] = useState("");
 
+  function handleOldPassChange(password) {
+    setOldPassword(password);
+    console.log(passwordStrength(password))
+  }
+
+  function handleNewPassChange(password) {
+    setNewPassword(password);
+  }
+  function handlePassAgainChange(password) {
+    setPasswordAgain(password);
+  }
+
+  return (
+        <>
+          <div id="mainContent" style={{}}>
+            <Form>
+              <Row>
+                <Form.Group as={Col}>
+                  <Form.Label>Current Password</Form.Label>
+                  <Form.Control
+                    id="passwordCur"
+                    className="search"
+                    type="password"
+                    placeholder="Enter Old Password"
+                    onChange={(event) => {
+                      handleOldPassChange(event.target.value);
+                    }}
+                  />
+                  <Form.Label>New Password</Form.Label>
+                  <Form.Control
+                    id="passwordNew"
+                    className="search"
+                    type="password"
+                    placeholder="Enter New Password"
+                    onChange={(event) => {
+                      handleNewPassChange(event.target.value);
+                    }}
+                  />
+                  <Form.Label>Verify Password</Form.Label>
+                  <Form.Control
+                    id="passwordNew2"
+                    className="search"
+                    type="password"
+                    placeholder="Verify New Password"
+                    onChange={(event) => {
+                      handlePassAgainChange(event.target.value);
+                    }}
+                  />
+                  <div id="btnContainer">
+                    <Button
+                      variant="primary"
+                      onClick={async () => {
+                        verifyPass(newPassword, passwordAgain)
+                          ? updatePassword(oldPassword, newPassword)
+                          : console.log("Passwords do not match");
+                      }}
+                    >
+                      Update Password
+                    </Button>
+                  </div>
+                </Form.Group>
+              </Row>
+            </Form>
+          </div>
+        </>
+  );
 }
-function ChangePassword() {
-    return(
-    <>
-        
-        <div id="mainContent"style={{ }}>
 
-                <Form>
-                    <Row>
-                        <Form.Group as={Col} >
-                            <Form.Label>Current Password</Form.Label>
-                            <Form.Control id="passwordCur" className="search" type="password" placeholder="Enter Old Password" />
-                            <Form.Label>New Password</Form.Label>
-                            <Form.Control id="passwordNew" className="search" type="password" placeholder="Enter New Password" />
-                            <Form.Label>Verify Password</Form.Label>
-                            <Form.Control id="passwordNew2" className="search" type="password" placeholder="Verify New Password" />
-                            <div id="btnContainer">
-                                <Button variant="primary" onClick={
-                                    async () => {
-                                    (verifyPass(document.getElementById('passwordNew').value, document.getElementById('passwordNew2').value) ? 
-                                       updatePassword(document.getElementById('passwordCur').value, document.getElementById('passwordNew').value): console.log("Passwords do not match") );
-                                        }} >  Update Password 
-                                </Button>
-                            </div>
-                        </Form.Group>
-
-                    </Row>
-                </Form>
-
-              
-                
-            
-        </div>
-
-    </>
-);
-}
-
-export default ChangePassword;
+// https://www.npmjs.com/package/check-password-strength
