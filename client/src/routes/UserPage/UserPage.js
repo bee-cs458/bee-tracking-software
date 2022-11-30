9f6298b5a882bc38fd2ab88bce0570ca242fe5ca
Ella Prew <prewe5294@my.uwstout.edu>
2022-11-30 15:05:18 -0600

    removed unused import

--- client/src/routes/UserPage/UserPage.js
+++ client/src/routes/UserPage/UserPage.js
@@ -5,7 +5,6 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import UserTable from "./../../components/UserTable/UserTable"
 
 function UserPage() {
 
