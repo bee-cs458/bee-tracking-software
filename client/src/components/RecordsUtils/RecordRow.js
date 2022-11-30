d011fd0e2313257dcb51dfe7c428f36128a77999
levertyk <levertyk4517@my.uwstout.edu>
2022-11-30 15:21:25 -0600

    beets-27 Added Record Table Component

---
+++ client/src/components/RecordsUtils/RecordRow.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import Form from "react-bootstrap/Form";
+import getAllRecords from "";
+
+export default function CheckInRow(props) {
+  const record = props.item;
+  const [damaged, setDamaged] = useState(false);
+  const [notes, setNotes] = useState(null);
+
+  useEffect(() => {}, [record]);
+
+  useEffect(() => {
+    record.operational = !damaged;
+  }, [damaged]);
+
+  useEffect(() => {
+    record.notes = notes;
+  }, [notes]);
+
+  function handleDamageChange(newVal) {
+    setDamaged(!damaged);
+  }
+
+  function handleNotesChange(newNotes) {
+    setNotes(newNotes);
+  }
+
+  return (
+    <tr>
+      <td>{record.record_tag}</td>
+      <td>{record.name}</td>
+      <td>{record.category}</td>
+      <td>{record.due_date}</td>
+    </tr>
+  );
+}
