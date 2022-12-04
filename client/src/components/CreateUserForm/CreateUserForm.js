5fd66a52d912d42bf807d2bb41bd451b60134fb7
PirateCody <gentzcody@gmail.com>
2022-12-04 02:59:53 -0600

    Cody Gentz - added basic validation and notifications

--- client/src/components/CreateUserForm/CreateUserForm.js
+++ client/src/components/CreateUserForm/CreateUserForm.js
@@ -5,6 +5,8 @@ import Col from "react-bootstrap/esm/Col";
 import { useState } from "react";
 import Button from "react-bootstrap/esm/Button";
 import { createNewUser } from "../../api/UserService";
+import Toast from 'react-bootstrap/Toast';
+import ToastContainer from 'react-bootstrap/ToastContainer';
 
 function CreateUserForm() {
 
@@ -20,49 +22,82 @@ function CreateUserForm() {
         advanced: 0
 
     });
+    const [toastData, setToastData] = useState({
+        title: "",
+        message: "",
+        variant: "",
+        show: false,
+    });
 
-    // TO DO
-    // 1. Error and success messages
-    // 2. Form validation
     const handleSubmit = async (event) => {
 
         event.preventDefault();
 
         await createNewUser(userData).then((result) => {
             if (result.status === 202) {
-                // good
-                console.log("Good Result: ")
+                setToastData({
+                    title: "Success",
+                    message: "User created successfully!",
+                    variant: "success",
+                    show: true
+                })
+                clearForm();
             } else {
-                // bad
-                console.log("Bad Result: ")
+                setToastData({
+                    title: "Error",
+                    message: "There was an error creating this user!",
+                    variant: "danger",
+                    show: true
+                })
             }
-
         });
-
     };
+
+    function clearForm() {
+
+        setUserData({
+            user_id: "",
+            first_name: "",
+            last_name: "",
+            username: "",
+            password: "",
+            permissions: -1,
+            advanced: 0
+        })
+
+    }
 
     return (
         <div>
             <h2>Creating a User</h2>
+
+            <ToastContainer position="bottom-center" className="p-5 m-5">
+                <Toast bg={toastData.variant} onClose={() => setToastData({ ...toastData, show: false })} show={toastData.show} delay={6000} autohide>
+                    <Toast.Header>
+                        <strong className="me-auto">{toastData.title}</strong>
+                    </Toast.Header>
+                    <Toast.Body className={"text-bg-" && toastData.variant}>{toastData.message}</Toast.Body>
+                </Toast>
+            </ToastContainer>
 
             <Form className="p-5 mb-5" onSubmit={handleSubmit}>
                 <Row className="mb-1">
                     <Form.Group as={Col} xs={4}>
 
                         <Form.Label>User ID</Form.Label>
-                        <Form.Control type="text" id="user_id" onChange={(event) => setUserData({ ...userData, user_id: parseInt(event.target.value) })} />
+                        <Form.Control required type="number" id="user_id" value={userData.user_id} onChange={(event) => setUserData({ ...userData, user_id: parseInt(event.target.value) })} />
 
                     </Form.Group>
                     <Form.Group as={Col} xs={4}>
 
                         <Form.Label>First Name</Form.Label>
-                        <Form.Control type="text" id="first_name" onChange={(event) => setUserData({ ...userData, first_name: event.target.value })} />
+                        <Form.Control required type="text" id="first_name" value={userData.first_name} onChange={(event) => setUserData({ ...userData, first_name: event.target.value })} />
 
                     </Form.Group>
                     <Form.Group as={Col} xs={4}>
 
                         <Form.Label>Last Name</Form.Label>
-                        <Form.Control type="text" id="last_name" onChange={(event) => setUserData({ ...userData, last_name: event.target.value })} />
+                        <Form.Control required type="text" id="last_name" value={userData.last_name} onChange={(event) => setUserData({ ...userData, last_name: event.target.value })} />
 
                     </Form.Group>
                 </Row>
@@ -70,21 +105,21 @@ function CreateUserForm() {
                     <Form.Group as={Col} xs={6}>
 
                         <Form.Label>Username</Form.Label>
-                        <Form.Control type="text" id="username" onChange={(event) => setUserData({ ...userData, username: event.target.value })} />
+                        <Form.Control required type="text" id="username" value={userData.username} onChange={(event) => setUserData({ ...userData, username: event.target.value })} />
 
                     </Form.Group>
                     <Form.Group as={Col} xs={6}>
 
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" id="password" onChange={(event) => setUserData({ ...userData, password: event.target.value })} />
+                        <Form.Control required type="password" id="password" value={userData.password} onChange={(event) => setUserData({ ...userData, password: event.target.value })} />
 
                     </Form.Group>
                 </Row>
-                <Row className="mb-2">
+                <Row className="mb-3">
                     <Form.Group as={Col} xs={6}>
 
                         <Form.Label>Permission Level</Form.Label>
-                        <Form.Select id="permissionsSelect" defaultValue="-1" onChange={(event) => setUserData({ ...userData, permissions: event.target.value })}>
+                        <Form.Select id="permissionsSelect" value={userData.permissions} onChange={(event) => setUserData({ ...userData, permissions: event.target.value })}>
                             <option value="-1">Guest</option>
                             <option value="0">Student</option>
                             <option value="1">Operator</option>
@@ -95,13 +130,18 @@ function CreateUserForm() {
                     <Form.Group as={Col} xs={6}>
 
                         <Form.Label>Advanced</Form.Label>
-                        <Form.Check type="switch" id="advancedSwitch" onChange={(event) => setUserData({ ...userData, advanced: (!userData.advanced ? 1 : 0) })} />
+                        <Form.Check type="switch" id="advancedSwitch" checked={userData.advanced} onChange={(event) => setUserData({ ...userData, advanced: (!userData.advanced ? 1 : 0) })} />
 
                     </Form.Group>
                 </Row>
                 <Row>
-                    <Col className="w-100">
+                    <Col className="w-100 mb-2">
                         <Button variant="primary" size="md" type="submit" className="w-100">Submit</Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="w-100">
+                        <Button variant="danger" size="md" type="button" className="w-100" onClick={clearForm}>Clear</Button>
                     </Col>
                 </Row>
             </Form>
