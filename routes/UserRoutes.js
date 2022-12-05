4befff31dd891537a465590c209dd2dffa02f047
ColbyTief <colbytief123@gmail.com>
2022-12-05 14:22:43 -0600

    BEETS-27 userRouteUpdate

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -6,5 +6,5 @@ import express from 'express';
 const router = express.Router();
 
 router.post("/update_password", restrictTo('operator'), requireBody('password', 'newPassword'), updateUserPassword);
-router.get("/get_by_id", restrictTo('operator'), requireBody('id'), getUserById);
+router.post("/get_by_id", restrictTo('operator'), requireBody('userId'), getUserById);
 export default router;
