c4ec63708d6ee5954411d1160779445aa756d938
ColbyTief <colbytief123@gmail.com>
2022-12-05 13:26:17 -0600

    BEETS-27 get user by id backend
    Routes and api for getting user by id setup

--- client/src/api/UserService.js
+++ client/src/api/UserService.js
@@ -1,27 +1,5 @@
 import axios from 'axios';
 
-export async function updatePass(pass, newPass) {
-    console.log("Sending change password request");
-    // construct query
-    const params = new URLSearchParams();
-    params.append('password', pass);
-    params.append('newPassword', newPass);
-    // do request
-    return axios({
-        method: 'POST',
-        url: '/api/user/update_password',
-        data : params.toString(),
-        withCredentials: true
-    }).then(
-        (response) => {
-            return response.data;
-        },
-        (err) => {
-            console.log("Issue updating password");
-            return err.response.data;
-        }
-    );
-}
 // current API in use for update password
 export async function updatePassword(password, newPassword){
     try {
@@ -36,3 +14,40 @@ export async function updatePassword(password, newPassword){
         return "Error Updating User Password from API";
     }
 }
+
+export async function getUserById(userId){
+    try {
+        console.log("Getting User by ID");
+
+        const response = await axios.post("/api/user/get_by_id", {
+            userId: userId
+        });
+        return response.data.result;
+    } catch (error) {
+        return "Error error getting user by ID";
+    }
+}
+
+// old function for updatePass(does not work)
+// export async function updatePass(pass, newPass) {
+//     console.log("Sending change password request");
+//     // construct query
+//     const params = new URLSearchParams();
+//     params.append('password', pass);
+//     params.append('newPassword', newPass);
+//     // do request
+//     return axios({
+//         method: 'POST',
+//         url: '/api/user/update_password',
+//         data : params.toString(),
+//         withCredentials: true
+//     }).then(
+//         (response) => {
+//             return response.data;
+//         },
+//         (err) => {
+//             console.log("Issue updating password");
+//             return err.response.data;
+//         }
+//     );
+// }
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,10 +1,10 @@
 import {
-    updateUserPassword
+    updateUserPassword, getUserById
 } from "../controllers/UserController.js";
 import { requireBody, restrictTo, requireQuery, filterQuery } from '../controllers/SecurityController.js'
 import express from 'express';
 const router = express.Router();
 
 router.post("/update_password", restrictTo('operator'), requireBody('password', 'newPassword'), updateUserPassword);
-
+router.get("/get_by_id", restrictTo('operator'), requireBody('id'), getUserById);
 export default router;
