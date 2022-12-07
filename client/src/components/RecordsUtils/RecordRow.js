b826b8e7b24b7a763e301a9d8a0a50acf8ce6895
levertyk <levertyk4517@my.uwstout.edu>
2022-12-07 12:42:27 -0600

    beets-27 MVP: Late records highlighted

--- client/src/components/RecordsUtils/RecordRow.js
+++ client/src/components/RecordsUtils/RecordRow.js
@@ -7,7 +7,6 @@ export default function CheckInRow(props) {
   const record = props.data;
   const [user, setUser] = useState({ first_name: "Loading User..." });
   const [asset, setAsset] = useState({ name: "Loading Asset..." });
-  const [overDue, setOverDue] = useState("notOverdue");
 
   const getInfo = async () => {
     await getUserById(record.student_id).then((result) => {
@@ -18,12 +17,15 @@ export default function CheckInRow(props) {
     });
   };
 
+  const dueDate = new Date(record.due_date);
+  // console.log(props.date);
+
   useEffect(() => {
     getInfo();
   }, []);
 
   return (
-    <tr className={overDue}>
+    <tr style={{ background: props.date > dueDate && !record.in_date ? "#ffb9b9" : "" }}>
       <td>{user.first_name + " " + user.last_name}</td>
       <td>{record.out_date}</td>
       <td>{asset.name}</td>
