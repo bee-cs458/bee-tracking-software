f2f8b2010618b97a6d8bd8184ef8e0a73004f46f
Rahmi Husein <huseinr2493@my.uwstout.edu>
2022-12-15 20:53:48 -0600

    partially completed beets-188

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,12 +1,32 @@
 import { query } from "../utilities/DatabaseUtilities.js";
 
-export default async (req, res, next) => {
-    await query( 'SELECT * FROM category'
-    ).then(
+export const getAllCategories = (req, res, next) =>
+    query( 'SELECT * FROM category').then(
         (result) => res.send({ result }),
         (reason) => {
             reason.message = `Error getting all categories: ${reason.message}`;
             next(reason);
         }
     )
-}
+
+
+export const createCategory = (req, res, next) => 
+    query(`INSERT INTO \`category\` (\`category_id\`,\`catName\`) VALUES (?,?)`, [req.params.id,req.body.catName]).then(
+        (result) => res.send({ result }),
+        (reason) => next(reason)
+    )
+
+export const deleteCategory = (req, res, next) => 
+    query(`DELETE FROM category WHERE category_id = ?`, [req.params.id]).then(
+        (result) => {
+            if (result.affectedRows <= 0) {
+                next({
+                    status: 404,
+                    message: "Category wasn't found!"
+                });
+                return;
+            }
+            res.send({ result })
+        },
+        (reason) => next(reason)
+    )
--- routes/CategoryRoutes.js
+++ routes/CategoryRoutes.js
@@ -1,8 +1,17 @@
 import express from 'express';
-import getAllCategories from '../controllers/CategoryController.js';
+import { getAllCategories, createCategory, deleteCategory} from '../controllers/CategoryController.js';
+
+import { restrictTo, requireBody } from '../controllers/SecurityController.js'
 
 const router = express.Router();
 
 router.get("/", getAllCategories);
+router.post(
+    "/:id",
+    restrictTo('operator'),
+    requireBody('catName'),
+    createCategory
+);
+router.delete("/:id", restrictTo('operator'), deleteCategory)
 
 export default router;
