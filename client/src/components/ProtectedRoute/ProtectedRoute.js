8cbd02ae7c626e18819f8ce79277232d86ec2267
Cody Gentz <gentzcody@gmail.com>
2023-02-08 14:56:41 -0600

    Restricted routes based on permissions

--- client/src/components/ProtectedRoute/ProtectedRoute.js
+++ client/src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,11 +1,13 @@
 import { Outlet, Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({
-    isAllowed,
+    requiredPermissionLevel,
+    userPermissionLevel,
     redirectPath = '/',
     children,
 }) => {
-    if (!isAllowed) {
+    if (userPermissionLevel < requiredPermissionLevel) {
+        console.log("User Permission: " + userPermissionLevel + "\nReq Permission: " + requiredPermissionLevel)
         return <Navigate to={redirectPath} replace />;
     }
 
