9a9c1d068e149f3e6908fe6fde61c38530fb6c59
PirateCody <gentzcody@gmail.com>
2023-02-28 21:32:45 -0600

    Integrated google login for deserializing user

--- utilities/PassportUtilities.js
+++ utilities/PassportUtilities.js
@@ -1,18 +1,40 @@
 import passport from "passport";
-import {query} from "./DatabaseUtilities.js";
-import {StrategyImplementation as googleAuth} from "./passport-google-auth.js";
-import {StrategyImplementation as localAuth} from "./passport-local-auth.js";
+import { query } from "./DatabaseUtilities.js";
+import { StrategyImplementation as googleAuth } from "./passport-google-auth.js";
+import { StrategyImplementation as localAuth } from "./passport-local-auth.js";
 
 passport.use(googleAuth);
 passport.use(localAuth);
 
-passport.serializeUser((user, done) => done(null, user.user_id));
+
+passport.serializeUser((user, done) => done(null, user));
 
 const FindUserQuery = (user_id) => query("SELECT * FROM `user` WHERE `user`.`user_id` = ?", [user_id]);
 
-passport.deserializeUser((user_id, done) => FindUserQuery(user_id).then(
-    (result) => done(null, result[0]),
-    (err) => done(err)
-));
+passport.deserializeUser((user, done) => {
+    if (user?.provider == "google") {
+
+        // In here, we need to query the database for a link between the google account 
+        // and a BEETS user
+
+        // If there is a BEETS user associated with the google account, we return that user information from the DB
+
+        // If there is not a BEETS account, we create one, then return that new user
+
+        console.log("login google");
+        done(null, user);
+    } else {
+        console.log("login local");
+        FindUserQuery(user.user_id).then(
+            (result) => done(null, result[0]),
+            (err) => done(err),
+        )
+    }
+
+});
+// passport.deserializeUser((user_id, done) => FindUserQuery(user_id).then(
+// (result) => done(null, result[0]),
+// (err) => done(err)
+// ));
 
 export default passport;
