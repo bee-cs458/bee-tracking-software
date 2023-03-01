450b96821f596b855bc748393dbdde99581b5a79
Cody Gentz <gentzcody@gmail.com>
2023-03-01 13:08:34 -0600

    Added comments and google to BEETS account link

--- utilities/PassportUtilities.js
+++ utilities/PassportUtilities.js
@@ -6,26 +6,31 @@ import { StrategyImplementation as localAuth } from "./passport-local-auth.js";
 passport.use(googleAuth);
 passport.use(localAuth);
 
-
 passport.serializeUser((user, done) => done(null, user));
 
-const FindUserQuery = (user_id) => query("SELECT * FROM `user` WHERE `user`.`user_id` = ?", [user_id]);
+const findUser = (user_id) => query("SELECT * FROM `user` WHERE `user`.`user_id` = ?", [user_id]);
+const matchUserEmail = (email) => query("SELECT * FROM `user` WHERE `user`.`email` = ?", [email]);
 
 passport.deserializeUser((user, done) => {
+    // If the user information we recieve is a google user, then search for the BEETS account
+    // using the Google account email
     if (user?.provider == "google") {
 
-        // In here, we need to query the database for a link between the google account 
-        // and a BEETS user
+        // Query the database to match the google account to a BEETS account (using their email)
+        matchUserEmail(user?.emails[0].value).then(
+            // If a BEETS account is found, returns that BEETS account data
+            (result) => done(null, result[0]),
+            // If one is not found, return an error
+            // TODO: create a new account and associate it with the google account
+            //  by setting the email equal to the email in the google account
+            (err) => done(err),
+        )
 
-        // If there is a BEETS user associated with the google account, we return that user information from the DB
-
-        // If there is not a BEETS account, we create one, then return that new user
-
-        console.log("login google");
-        done(null, user);
+        // If the user information we recieve is not a Google user, then it is a regular
+        // BEETS account - proceed with getting the user data from the DB as normal
     } else {
         console.log("login local");
-        FindUserQuery(user.user_id).then(
+        findUser(user.user_id).then(
             (result) => done(null, result[0]),
             (err) => done(err),
         )
