ae32e1d50ee30167e2d11272f77c609efefd98de
levertyk <levertyk4517@my.uwstout.edu>
2023-03-01 15:21:22 -0600

    Revert "beets-269 Made records page items have a dropdown"
    This reverts commit cd5f4292e86e938470bfbfcba1a64ba352dffecd.

--- client/src/components/RecordsUtils/RecordRow.js
+++ client/src/components/RecordsUtils/RecordRow.js
@@ -1,12 +1,7 @@
 import "./RecordRow.css";
-import { useState } from "react";
 
 export default function CheckInRow(props) {
   const record = props.record;
-  const [expanded, setExpanded] = useState(false);
-  const toggleExpanded = () => {
-    setExpanded(!expanded);
-  };
 
   const dueDate = new Date(record.due_date);
 
@@ -21,83 +16,22 @@ export default function CheckInRow(props) {
     }
   }
 
-  const rowClicked = () => {
-    toggleExpanded();
-  };
-
-  /**
-   * <td>{props.userName}</td>
+  return (
+    <tr
+      style={{
+        background: record.in_date === null ? (props.date > dueDate && !record.in_date ? "#ffb9b9" : "#55ff77") :  "",
+      }}
+    >
+      <td>{props.userName}</td>
+      <td>{record.student_id}</td>
       <td>{props.opName}</td>
+      <td>{record.operator_id}</td>
       <td>{formatDate(record.out_date)}</td>
+      <td>{record.asset_tag}</td>
       <td>{props.assetName}</td>
+      <td>{formatDate(record.due_date)}</td>
       <td>{formatDate(record.in_date)}</td>
       <td>{record.notes}</td>
-   */
-
-  return (
-    <>
-      <tr
-        style={{
-          background:
-            record.in_date === null
-              ? props.date > dueDate && !record.in_date
-                ? "#ffb9b9"
-                : "#55ff77"
-              : "",
-        }}
-        onClick={() => rowClicked()}
-      >
-        <td>{record.record_id}</td>
-        <td>{record.asset_tag}</td>
-        <td>{record.student_id}</td>
-        <td>{record.operator_id}</td>
-        <td>{formatDate(record.due_date)}</td>
-      </tr>
-      {expanded ? (
-        <tr className="dropdown">
-          <td colSpan="5">
-            <div className="container-fluid">
-              <div className="row">
-                <div className="col">
-                  <span>
-                    <b>Asset: </b>
-                  </span>
-                  {props.assetName}
-                  <br />
-                  <span>
-                    <b>Student: </b>
-                  </span>
-                  {props.userName}
-                  <br />
-                  <span>
-                    <b>Operator: </b>
-                  </span>
-                  {props.opName}
-                </div>
-                <div className="col">
-                  <span>
-                    <b>Checked Out:</b>
-                  </span>
-                  {formatDate(record.out_date)}
-                  <br />
-                  <span>
-                    <b>Checked In:</b>
-                  </span>
-
-                  {record.in_date ? formatDate(record.in_date) : <>Still out</>}
-                </div>
-              </div>
-            </div>
-            <span>
-              <b>Notes: </b>
-            </span>
-            {props.notes}
-            <br />
-            {props.damageNotes}
-            {}
-          </td>
-        </tr>
-      ) : null}
-    </>
+    </tr>
   );
 }
