0279efeb88862e815ed5ec163f405d0fa2711bae
Cody Gentz <gentzcody@gmail.com>
2023-03-13 21:20:01 -0500

    BEETS-252 changing access control to minimum rank

--- client/src/components/AccessControl/AccessControl.js
+++ client/src/components/AccessControl/AccessControl.js
@@ -5,7 +5,7 @@ export const AccessControl = ({allowedRank, children, renderNoAccess}) => {
 
     const [userState, setUserState] = useContext(GlobalStateContext);
 
-    const permitted = userState.permissions === allowedRank;
+    const permitted = userState.permissions >= allowedRank;
 
     if (permitted) {
       return children;
