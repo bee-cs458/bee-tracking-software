826d8bb85bb650bfe3c42fbd52ad1c4d56fe57c2
levertyk <levertyk4517@my.uwstout.edu>
2023-03-20 14:15:45 -0500

    beets-48 began implementation

--- client/src/routes/RecordsPage/RecordsPage.js
+++ client/src/routes/RecordsPage/RecordsPage.js
@@ -1,13 +1,16 @@
 import RecordTable from "../../components/RecordsUtils/RecordTable.js";
 import { useOutletContext } from "react-router-dom";
+import CheckedOut from "../../components/CheckedOut.js";
 
 export default function RecordsPage() {
   const [theme, setTheme] = useOutletContext();
+  const [checked, setChecked] = useState(false);
 
   return (
     <>
       <div className="header-container"></div>
       <div className="main-content">
+        <CheckedOut></CheckedOut> {/*todo, add parameters here */}
         <RecordTable variant={theme}></RecordTable>
       </div>
     </>
