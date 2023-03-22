ec72533f8ba209684b67e08ad09022ea4901ac85
Cody Gentz <you@example.com>
2023-03-22 12:57:59 -0500

    BEETS-310 Added unit tests to login functions

--- client/src/api/AuthService.js
+++ client/src/api/AuthService.js
@@ -1,49 +1,47 @@
-import axios from 'axios';
+import axios from "axios";
 
 export function verifyLogin(user, pass) {
-    //console.log("Sending login request");
-    // construct query
-    const params = new URLSearchParams();
-    params.append('username', user);
-    params.append('password', pass);
-    // do request
-    return axios({
-        method: 'POST',
-        url: '/api/login',
-        data: params.toString(),
-        withCredentials: true
-    }).then(
-        (response) => {
-            return response;
-        },
-        (err) => {
-            return {
-                status: err.response.status,
-            };
-        }
-    );
+  // construct query
+  const params = new URLSearchParams();
+  params.append("username", user);
+  params.append("password", pass);
+  // do request
+  return axios({
+    method: "POST",
+    url: "/api/login",
+    data: params.toString(),
+    withCredentials: true,
+  }).then(
+    (response) => {
+      return response;
+    },
+    (err) => {
+      return {
+        status: err.response.status,
+      };
+    }
+  );
 }
 
 export function getLoggedInUser() {
-
-    return axios({
-        method: "GET",
-        url: "/api/login/success",
-    }).then(
-        (response) => {
-            return response;
+  return axios({
+    method: "GET",
+    url: "/api/login/success",
+  }).then(
+    (response) => {
+      return response;
+    },
+    (err) => {
+      return {
+        user: {
+          user_id: -1,
+          permissions: -1,
         },
-        (err) => {
-            return {
-                user: {
-                    user_id: -1,
-                    permissions: -1
-                }
-            };
-        }
-    );
+      };
+    }
+  );
 }
 
 export function triggerLogout() {
-    axios.post("/api/login/logout", { withCredentials: true });
+  axios.post("/api/login/logout", { withCredentials: true });
 }
