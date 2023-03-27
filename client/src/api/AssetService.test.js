3bfb0a731b95be9ed313ccfaf57af99bc3dac738
ColbyTief <colbytief123@gmail.com>
2023-03-27 14:21:23 -0500

    beets-260 fixed formatting

--- client/src/api/AssetService.test.js
+++ client/src/api/AssetService.test.js
@@ -48,20 +48,31 @@ describe('get tests', () => {
             // tests
             Services.getAllAvailableAssets().then(data => expect(data).toEqual(assets));
             expect(axios.get).toHaveBeenCalledWith("/api/asset/get_available");
-        })
+        });
 
 
         it('should return an error', async () => {
             axios.get.mockImplementation(() => {
                 throw new Error();});
             expect(await Services.getAllAvailableAssets()).toBe("Error Getting Available Assets from API");
-        })
+        });
 
     });
 
+});
+
+//    //Get all unavailable assets from API and return them
+//     static async getAllUnavailableAssets() {
+//         try {
+//             const response = await axios.get("/api/asset/get_unavailable");
+//             return response.data;
+//         } catch (error) {
+//             return "Error Getting Unavailable Assets from API";
+//         }
+//     };
 
 
-    describe('getAllUnavailableAssets tests', () => {
+ describe('getAllUnavailableAssets tests', () => {
         it('should return all unavailable assets', async () => {
 
 
@@ -82,40 +93,88 @@ describe('get tests', () => {
 
 
 
-    describe('getAllAssetsWithDueDates tests', () => {
-        it('should return all assets with due dates', async () => {
+ describe('getAllAssetsWithDueDates tests', () => {
+    it('should return all assets with due dates', async () => {
+        axios.get.mockResolvedValueOnce(response);
+        Services.getAllAssetsWithDueDates().then(data => expect(data).toEqual(assets));
+        expect(axios.get).toHaveBeenCalledWith("/api/asset/get_all_due_dates");
+    })
 
+    it('should return an error', async() => {
+        axios.get.mockImplementation(() => {
+            throw new Error();
+        }); 
+        
+        expect(await Services.getAllAssetsWithDueDates()).toBe("Error Getting Assets with Due Dates from API");
+    })
+});
+describe("getAssetsByDescription tests", () => {
+  it("should return test model and check the get call", async () => {
+    axios.get.mockResolvedValueOnce(response);
 
-            axios.get.mockResolvedValueOnce(response);
-            Services.getAllAssetsWithDueDates().then(data => expect(data).toEqual(assets));
-            expect(axios.get).toHaveBeenCalledWith("/api/asset/get_all_due_dates");
-        })
+    Services.getAssetsByDescription("butter").then((data) =>
+      expect(data).toEqual(assets)
+    );
 
-        it('should return an error', async() => {
-
-            axios.get.mockImplementation(() => {
-                throw new Error();
-            }); 
-            
-            expect(await Services.getAllAssetsWithDueDates()).toBe("Error Getting Assets with Due Dates from API");
-        })
+    expect(axios.get).toHaveBeenCalledWith("/api/asset/search", {
+      params: {
+        limit: 10,
+        description: "butter",
+      },
     });
+  });
+});
 
+describe('getAssetsByDescription tests', () => {
+    it('should return test model and check the get call', async () => {
+   
+        axios.get.mockResolvedValueOnce(response);
 
+   
+        Services.getAssetsByDescription("butter").then(data => expect(data).toEqual(assets));
 
-    describe('getAssetsByDescription tests', () => {
-        it('should return test model and check the get call', async () => {
-            
-            axios.get.mockResolvedValueOnce(response);
-            Services.getAssetsByDescription("butter").then(data => expect(data).toEqual(assets));
-            expect(axios.get).toHaveBeenCalledWith("/api/asset/search", {
-                params: {
-                  limit: 10,
-                  description: "butter",
-                },
-              });
+        expect(axios.get).toHaveBeenCalledWith("/api/asset/search", {
+            params: {
+              limit: 10,
+              description: "butter",
+            },
+          });
 
-        })
+    })
+
+describe("getAssetsByDescription tests", () => {
+  it("should return test model and check the get call", async () => {
+    axios.get.mockResolvedValueOnce(response);
+
+    Services.getAssetsByDescription("butter").then((data) =>
+      expect(data).toEqual(assets)
+    );
+
+    expect(axios.get).toHaveBeenCalledWith("/api/asset/search", {
+      params: {
+        limit: 10,
+        description: "butter",
+      },
+    });
+  });
+});
+});
+describe('getAssetsByDescription tests', () => {
+    it('should return test model and check the get call', async () => {
+   
+        axios.get.mockResolvedValueOnce(response);
+
+   
+        Services.getAssetsByDescription("butter").then(data => expect(data).toEqual(assets));
+
+        expect(axios.get).toHaveBeenCalledWith("/api/asset/search", {
+            params: {
+              limit: 10,
+              description: "butter",
+            },
+          });
+
+    })
 
         it('should return an error', async() => {
 
@@ -136,47 +195,56 @@ describe('get tests', () => {
 
         })
 
-        it('should return an error', async() => {
-            const error = [response];
+        // it('should return an error', async() => {
+        //     const error = [response];
         
-            axios.get.mockImplementation(() => {
-                throw new Error();
-            }); 
+        //     axios.get.mockImplementation(() => {
+        //         throw new Error();
+        //     }); 
             
-            expect(await Services.getAssetFromCat()).toBe("Error Getting Assets in category from API");
-        })
+        //     expect(await Services.getAssetFromCat()).toBe("Error Getting Assets in category from API");
+        // })
 
-        it('should return a 404 error', async() => {
+        // it('should return a 404 error', async() => {
 
-            error.response.status = 404;
-            error.response.data.result = "rats";
-            axios.get.mockResolvedValueOnce(error); 
+        //     error.response.status = 404;
+        //     error.response.data.result = "rats";
+        //     axios.get.mockResolvedValueOnce(error); 
             
-            expect(await Services.getAssetFromCat()).toBe("rats");
-        })
+        //     expect(await Services.getAssetFromCat()).toBe("rats");
+        // })
 
-    });
+    //});
 
 });
 
-describe('create/edit/delete tests', () => {
+// describe('create/edit/delete tests', () => {
     
-    describe('getAllAssets tests', () => {
-        it('should return all assets', async () => {
-            // replace normal get response with test values
-            axios.get.mockResolvedValueOnce(response);
-            // tests
-            Services.getAllAssets().then(data => expect(data).toEqual(assets));
-            expect(axios.get).toHaveBeenCalledWith("/api/asset/get_all");
-        })
+//     describe('Create New Asset', () => {
+//         it('should call axios.post with new assset info', async () => {
+//             // replace normal get response with test values
+//             // tests
+            
+//             //Services.createNewAsset("KOO-KOO-BER","Laughing KooKoobera", "He laughs at you","Creature",1,1);
+//             // expect(axios.post).toHaveBeenCalledWith("/api/asset/", {
+//             //     asset_tag: "KOO-KOO-BER",
+//             //     name: "Laughing KooKoobera",
+//             //     description: "He laughs at you",
+//             //     date_added: date, //Set date to todays date (FIX)
+//             //     category: "Creature",
+//             //     operational: 1,
+//             //     advanced: 2,
+//             //     checked_out: 0, //Automatically set to not checked out
+//             // });
+//         })
 
 
-        it('should return an error', async () => {
-            axios.get.mockImplementation(() => {
-                throw new Error();});
-            expect(await Services.getAllAssets()).toBe("Error Getting Assets from API");
-        })
+//         // it('should return an error', async () => {
+//         //     axios.get.mockImplementation(() => {
+//         //         throw new Error();});
+//         //     expect(await Services.getAllAssets()).toBe("Error Getting Assets from API");
+//         // })
 
-    });
+//     });
 
-});
+// });
