79622b814039ac38e9c2580295922d08f398e396
jwehrman25 <poisondarttamer@gmail.com>
2023-03-28 08:31:52 -0500

    beets-181 added comments and formatting

--- client/src/components/PasswordAlert/PasswordAlert.js
+++ client/src/components/PasswordAlert/PasswordAlert.js
@@ -1,8 +1,13 @@
-
 export default function PasswordAlert(props) {
-  if(props.message !== "") {
-    return <div className="alert alert-danger "role="alert" dangerouslySetInnerHTML={{__html: props.message}} ></div>
-  }
-  else
-      return;
+  if (props.message !== "") {
+    //the componet won't appear unless there is a message in it.
+    return (
+      <div //bootstrap alert div class and role
+        className="alert alert-danger "
+        role="alert"
+        dangerouslySetInnerHTML={{ __html: props.message }} 
+        //the above thing is what allows for the <br> html element work within the message.
+      ></div>
+    );
+  } else return;
 }
