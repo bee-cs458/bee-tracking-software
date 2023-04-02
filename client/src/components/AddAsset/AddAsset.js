7c22c3a69b76e86c584931c5027dfdd91486b5b4
levertyk <levertyk4517@my.uwstout.edu>
2023-04-02 15:13:13 -0500

    beets-433 Finished Conversion to Full React

--- client/src/components/AddAsset/AddAsset.js
+++ client/src/components/AddAsset/AddAsset.js
@@ -1,234 +1,140 @@
-import Button from "react-bootstrap/esm/Button";
-import * as React from "react";
-import { useState } from "react";
-import "./AddAsset.css";
+import React, { useState } from "react";
 import { createNewAsset } from "../../api/AssetService";
 import ConditionalAlert from "../CheckInUtilities/ConditionalAlert";
-import { Row, Col, Form, DropdownButton } from "react-bootstrap/esm/";
-import Dropdown from "react-bootstrap/Dropdown";
-import DropdownItem from "react-bootstrap/esm/DropdownItem";
-import DropdownToggle from "react-bootstrap/esm/DropdownToggle";
-import DropdownMenu from "react-bootstrap/esm/DropdownMenu";
+import { Form, Button, Row } from "react-bootstrap";
 import { useOutletContext } from "react-router-dom";
+import "./AddAsset.css";
 
-function AddAsset(props) {
-  //Pass in list of categories
-  const cats = props.cats;
-  const [category, setCategory] = useState(null);
-
-  const [advancedChecked, setAdvancedChecked] = React.useState(false);
-  const [operationalChecked, setOperationalChecked] = React.useState(true);
-  const [disabledState, setSubmitBtnDisabled] = React.useState(false);
+export default function AddAsset({ cats }) {
+  const [category, setCategory] = useState(cats[0]?.category_id);
+  const [advancedChecked, setAdvancedChecked] = useState(false);
+  const [operationalChecked, setOperationalChecked] = useState(true);
+  const [assetTag, setAssetTag] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [disabledState, setSubmitBtnDisabled] = useState(false);
   const [alertType, setAlertType] = useState(null);
   const [alertMessage, setAlertMessage] = useState(null);
-
   const [theme] = useOutletContext();
 
-  const changeAdvanced = () => {
-    setAdvancedChecked(!advancedChecked);
+  const handleCatChange = (newVal) => {
+    setCategory(newVal);
+    setAlertMessage(null);
+    setAlertType(null);
   };
 
-  const changeOperational = () => {
-    setOperationalChecked(!operationalChecked);
-  };
-
-  return (
-    <>
-      <Row className="m-3">
-        <ConditionalAlert
-          type={alertType}
-          message={alertMessage}
-        ></ConditionalAlert>
-      </Row>
-
-      <div class="AddAsset">
-        {/* <h6>Asset Tag:</h6>
-        <input maxLength="20" type="text" id="assetTag" name="assetTag"></input>
-        <br></br>
-        <br></br>
-
-        <h6>Name:</h6>
-        <input type="text" id="assetName" name="assetName"></input>
-        <br></br>
-        <br></br>
-
-        <h6>Description:</h6>
-        <textarea
-          rows="2"
-          cols="50"
-          id="description"
-          name="description"
-        ></textarea>
-        <br></br>
-        <br></br>*/}
-
-        {/* <div id="div1">
-          <h6>Select Category</h6>
-          <select
-            defaultValue={cats[0].category_id}
-            onChange={(event) => {
-              handleCatChange(event.target.value);
-            }}
-          >
-            {cats.map((cat) => (
-              <option value={cat.category_id}>{cat.catName}</option>
-            ))}
-          </select>
-        </div> */}
-
-        {/* <div id="div2">
-          <h6>Advanced Status</h6>
-          <input
-            id="advanced"
-            type="checkbox"
-            checked={advancedChecked}
-            onChange={changeAdvanced}
-          />
-        </div> */}
-        {/* <div id="div3">
-          <h6>Operational Status</h6>
-          <input
-            id="operational"
-            type="checkbox"
-            checked={operationalChecked}
-            onChange={changeOperational}
-          />
-          <br></br>
-          <br></br>
-        </div> */}
-
-        <Form>
-          <Col>
-            <Form.Group>
-              <Form.Label>Asset Tag:</Form.Label>
-              <Form.Control
-                type="text"
-                id="assetTag"
-                name="assetTag"
-              ></Form.Control>
-            </Form.Group>
-
-            <Form.Group>
-              <Form.Label>Name:</Form.Label>
-              <Form.Control
-                type="text"
-                id="assetName"
-                name="assetName"
-              ></Form.Control>
-            </Form.Group>
-
-            <Form.Group>
-              <Form.Label>Description:</Form.Label>
-              <Form.Control
-                type="text-area"
-                id="description"
-                name="description"
-                lines="3"
-              ></Form.Control>
-            </Form.Group>
-
-            <Form.Group>
-              <Form.Label>Select Category</Form.Label>
-              <Dropdown title="Select Category" onSelect={handleCatChange}>
-                <DropdownToggle>
-                  {cats[category]?.catName || "Select Category"}
-                </DropdownToggle>
-                <DropdownMenu variant={theme}>
-                  {cats.map((cat) => (
-                    <DropdownItem eventKey={cat.category_id}>
-                      {cat.catName}
-                    </DropdownItem>
-                  ))}
-                </DropdownMenu>
-              </Dropdown>
-            </Form.Group>
-
-            <Form.Group>
-              <Form.Label dark>Advanced Status</Form.Label>
-              <Form.Check
-                inline
-                type="switch"
-                id="advanced"
-                checked={advancedChecked}
-                onChange={changeAdvanced}
-              ></Form.Check>
-              <Form.Label>Operational:</Form.Label>
-              <Form.Check
-                inline
-                type="switch"
-                id="operational"
-                checked={operationalChecked}
-                onChange={changeOperational}
-              ></Form.Check>
-            </Form.Group>
-          </Col>
-        </Form>
-
-        <Button
-          variant="primary"
-          className="beets_buttons"
-          id="submitButton"
-          disabled={disabledState}
-          onClick={getInputValues}
-        >
-          Submit
-        </Button>
-      </div>
-    </>
-  );
-
-  function getInputValues() {
-    let assetTag = document.getElementById("assetTag").value;
-    let name = document.getElementById("assetName").value;
-    let description = document.getElementById("description").value;
-
-    //Validate user inputs
-    if (assetTag !== "" && name !== "" && description !== "") {
+  const handleSubmit = async () => {
+    if (assetTag && name && description) {
       console.log("Passed Validation");
-      // disable submit btn
       setSubmitBtnDisabled(true);
 
-      let advanced = advancedChecked ? 1 : 0;
-      let operational = operationalChecked ? 1 : 0;
+      const advanced = advancedChecked ? 1 : 0;
+      const operational = operationalChecked ? 1 : 0;
 
-      //Create Asset with given values
-      createNewAsset(
-        assetTag,
-        name,
-        description,
-        category,
-        operational,
-        advanced
-      )
-        .then(
-          (result) => {
-            console.log(result);
-            setAlertMessage("Asset has been Successfully Created!");
-            setAlertType(3);
-            //handleClose();
-          },
-          (err) => {
-            setAlertType(1);
-            setAlertMessage("Failed to Create New Asset!");
-            console.log(err);
-          }
-        )
-        .finally(
-          // reenable the submit button
-          () => setSubmitBtnDisabled(false)
+      try {
+        const result = await createNewAsset(
+          assetTag,
+          name,
+          description,
+          category,
+          operational,
+          advanced
         );
+        console.log(result);
+        setAlertMessage("Asset has been Successfully Created!");
+        setAlertType(3);
+      } catch (err) {
+        setAlertType(1);
+        setAlertMessage("Failed to Create New Asset!");
+        console.log(err);
+      } finally {
+        setSubmitBtnDisabled(false);
+      }
     } else {
       console.log("All Fields required");
       setAlertType(1);
       setAlertMessage("All fields must be filled out to create an asset");
     }
-  }
+  };
 
-  function handleCatChange(newVal) {
-    setCategory(newVal);
-    setAlertMessage(null);
-    setAlertType(null);
-  }
+  return (
+    <>
+      <Row className="m-3">
+        <ConditionalAlert type={alertType} message={alertMessage} />
+      </Row>
+
+      <div className="AddAsset">
+        <Form>
+          <Form.Group>
+            <Form.Label>Asset Tag:</Form.Label>
+            <Form.Control
+              maxLength="20"
+              type="text"
+              id="assetTag"
+              name="assetTag"
+              onChange={(event) => setAssetTag(event.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Name:</Form.Label>
+            <Form.Control
+              type="text"
+              id="assetName"
+              name="assetName"
+              onChange={(event) => setName(event.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Description:</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows="2"
+              cols="50"
+              id="description"
+              name="description"
+              onChange={(event) => setDescription(event.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Select Category:</Form.Label>
+            <Form.Control
+              as="select"
+              defaultValue={cats[0].category_id}
+              onChange={(event) => handleCatChange(event.target.value)}
+            >
+              {cats.map((cat) => (
+                <option key={cat.category_id} value={cat.category_id}>
+                  {cat.catName}
+                </option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Advanced Status:</Form.Label>
+            <Form.Check
+              type="checkbox"
+              id="advanced"
+              onChange={() => setAdvancedChecked(!advancedChecked)}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Operational Status:</Form.Label>
+            <Form.Check
+              type="checkbox"
+              id="operational"
+              onChange={() => setOperationalChecked(!operationalChecked)}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </>
+  );
 }
-
-export default AddAsset;
