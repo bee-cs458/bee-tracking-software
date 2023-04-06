31d12eb52364dd00dc786e6d08d38e6ea41b5f92
jwehrman25 <poisondarttamer@gmail.com>
2023-04-05 21:36:21 -0500

    beets-463 added button and modal to profile page

--- client/src/routes/ProfilePage/ProfilePage.js
+++ client/src/routes/ProfilePage/ProfilePage.js
@@ -1,8 +1,19 @@
 import ChangePassword from "../../components/ChangePassword/ChangePassword";
 import { AccountLink } from "../../components/AccountLink/AccountLink";
 import "./ProfilePage.css";
+import { Button, Modal } from "react-bootstrap";
+import { useState } from "react";
+//import { useOutletContext } from "react-router-dom";
 
 export default function ProfilePage() {
+  const [show, setShow] = useState(false);
+  //const [theme, setTheme] = useOutletContext();
+  const handleClose = () => setShow(false);
+  const handleShow = () => {
+    setShow(true);
+    sessionStorage.clear();
+  };
+
   return (
     <div>
       <div className="header-container">
@@ -11,9 +22,23 @@ export default function ProfilePage() {
         </div>
       </div>
       <div className="main-content">
-        <h1>Change Password</h1>
-        <ChangePassword></ChangePassword>
+        <Button
+          className="beets_buttons"
+          onClick={() => {
+            handleShow();
+          }}
+        >
+          Change Password
+        </Button>
       </div>
+      <Modal show={show} onHide={handleClose} backdrop="static" >
+        <Modal.Header closeButton>
+          <Modal.Title>Change Password</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ChangePassword></ChangePassword>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
