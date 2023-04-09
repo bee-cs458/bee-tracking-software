b01587c7c20960c0aaa37de7d020c30436143854
PirateCody <gentzcody@gmail.com>
2023-04-08 21:08:58 -0500

    BEETS-447 added layout and style to SAP

--- client/src/routes/SingleAssetPage/SingleAssetPage.js
+++ client/src/routes/SingleAssetPage/SingleAssetPage.js
@@ -1,11 +1,16 @@
+import { faCircleCheck, faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { useParams, useLocation } from "react-router-dom";
 
 import { getAssetByAssetTag } from "../../api/AssetService";
 import { getCategoryById } from "../../api/CategoryService";
+import { AccessControl } from "../../components/AccessControl/AccessControl";
 import { AccountLink } from "../../components/AccountLink/AccountLink";
 import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
+import RecordTable from "../../components/RecordsUtils/RecordTable";
+import { Ranks } from "../../constants/PermissionRanks";
 import { SIDE_NAV_WIDTH, TOP_BAR_HEIGHT } from "../../constants/StyleConstants";
 import styles from "./SingleAssetPage.css"
 
@@ -71,10 +76,94 @@ const SingleAssetPage = () => {
             <>
               <h2>{asset.name}</h2>
               <div className="seperator" />
-              <p>Asset Tag: {asset.asset_tag}</p>
-              <p>Description: {asset.description}</p>
-              <p>Damage Notes: {asset.damage_notes}</p>
-              <p>Category: {categoryLabel}</p>
+              <Row>
+                <Col xs={8}>
+                  <table className="w-100">
+                    <tr>
+                      <th>Asset Tag</th>
+                      <td>{asset.asset_tag}</td>
+                    </tr>
+                    <tr>
+                      <th>Category</th>
+                      <td>{categoryLabel}</td>
+                    </tr>
+                    <tr>
+                      <th>Date Added</th>
+                      <td>{asset.date_added}</td>
+                    </tr>
+                    <tr>
+                      <th>Description</th>
+                      <td>{asset.description}</td>
+                    </tr>
+                  </table>
+                </Col>
+                <Col xs={1} />
+                <Col xs={2}>
+                  <table className="w-100">
+                    <tr>
+                      <th>Operational</th>
+                      <td>{asset.operational ? <FontAwesomeIcon icon={faCircleCheck} className={"icon green"} /> : <FontAwesomeIcon icon={faCircleXmark} className={"icon red"} />}</td>
+                    </tr>
+                    <tr>
+                      <th>Available</th>
+                      <td>{!asset.checked_out ? <FontAwesomeIcon icon={faCircleCheck} className={"icon green"} /> : <FontAwesomeIcon icon={faCircleXmark} className={"icon red"} />}</td>
+                    </tr>
+                    <tr>
+                      <th>Advanced</th>
+                      <td>{asset.advanced ? <FontAwesomeIcon icon={faCircleCheck} className={"icon blue"} /> : <FontAwesomeIcon icon={faCircleXmark} className={"icon gray"} />}</td>
+                    </tr>
+                  </table>
+                </Col>
+                <Col xs={1} />
+              </Row>
+              <Row className="mt-3">
+                <Col>
+                  <table>
+                    <tr>
+                      <th>Damage Notes</th>
+                    </tr>
+                    <tr>
+                      <td>{asset.damage_notes === null ? "No damage notes have been recorded. " : asset.damage_notes}</td>
+                    </tr>
+                  </table>
+                </Col>
+              </Row>
+
+              <div className="seperator" />
+
+              <div>
+                <AccessControl allowedRank={Ranks.OPERATOR}>
+                  <h3>Checkout History</h3>
+                  <div className="seperator" />
+                  <table className="w-100">
+                    <tr>
+                      <th>Record ID</th>
+                      <th>Asset Tag</th>
+                      <th>Student ID</th>
+                      <th>Operator ID</th>
+                      <th>Due Date</th>
+                    </tr>
+                    <tr>
+                      <td>Asset Tag</td>
+                      <td>{asset.asset_tag}</td>
+                    </tr>
+                    <tr>
+                      <td>Category</td>
+                      <td>{categoryLabel}</td>
+                    </tr>
+                    <tr>
+                      <td>Date Added</td>
+                      <td>{asset.date_added}</td>
+                    </tr>
+                    <tr>
+                      <td>Description</td>
+                      <td>{asset.description}</td>
+                    </tr>
+                  </table>
+                </AccessControl>
+              </div>
+
+
             </>
           )}
 
