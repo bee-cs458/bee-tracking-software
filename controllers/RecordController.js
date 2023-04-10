2f65078b43c91d5001b76f6fa87b2ec25fc9200a
Cody Gentz <you@example.com>
2023-04-10 09:01:55 -0500

    BEETS-447 added param parsing to get records backend

--- controllers/RecordController.js
+++ controllers/RecordController.js
@@ -1,22 +1,31 @@
 import { query } from "../utilities/DatabaseUtilities.js";
 
 export const getAllRecords = async (req, res, next) => {
-    await query(`SELECT * FROM checkoutrecord ORDER BY record_id DESC;`).then(
-        (result) => res.send({ result }),
-        (reason) => {
-            reason.message = `Error Getting All records: ${reason.message}`;
-            next(reason);
-        }
-    )
-}
+  let statement = `SELECT * FROM checkoutrecord ORDER BY record_id DESC;`;
+
+  console.log(JSON.stringify(req.params));
+
+  if (req.params.assetTag) {
+    statement = `SELECT * FROM checkoutrecord WHERE asset_tag=${req.params.assetTag} ORDER BY record_id DESC;`;
+  }
+
+  await query(statement).then(
+    (result) => res.send({ result }),
+    (reason) => {
+      reason.message = `Error Getting All records: ${reason.message}`;
+      next(reason);
+    }
+  );
+};
 
 export const getAllCheckedOutRecords = async (req, res, next) => {
-    await query(`SELECT * FROM checkoutrecord ORDER BY record_id DESC WHERE in_date = null;`).then(
-        (result) => res.send({ result }),
-        (reason) => {
-            reason.message = `Error Getting checked out records: ${reason.message}`;
-            next(reason);
-        }
-    )
-}
-
+  await query(
+    `SELECT * FROM checkoutrecord ORDER BY record_id DESC WHERE in_date = null;`
+  ).then(
+    (result) => res.send({ result }),
+    (reason) => {
+      reason.message = `Error Getting checked out records: ${reason.message}`;
+      next(reason);
+    }
+  );
+};
