877ba98fef4bd1609e608411d22a658840014668
PirateCody <gentzcody@gmail.com>
2023-04-11 00:54:42 -0500

    BEETS-447 Comments

--- client/src/api/RecordService.js
+++ client/src/api/RecordService.js
@@ -2,10 +2,13 @@ import axios from "axios";
 
 /*
 Calls the API endpoint for getting all assets, does not include due dates
+@param assetTag - if we want all the records for a given asset, pass in an asset tag
 @return collection of assets from the database, where each entry is one row. 
 */
 export async function getAllRecords(assetTag) {
   try {
+    // If an asset tag is provided, pass that along in the request
+    // Otherwise, get all records from the database
     if (assetTag) {
       const response = await axios.get("/api/records/get_all", {
         params: { assetTag: assetTag },
--- controllers/RecordController.js
+++ controllers/RecordController.js
@@ -3,6 +3,8 @@ import { query } from "../utilities/DatabaseUtilities.js";
 export const getAllRecords = async (req, res, next) => {
   let statement = `SELECT * FROM checkoutrecord ORDER BY record_id DESC;`;
 
+  // Checks whether an asset tag was passed in from the frontend
+  // If there was, the query is changed to get all records for that specific asset tag
   if (req.query.assetTag) {
     statement = `SELECT * FROM checkoutrecord WHERE checkoutrecord.asset_tag=${req.query.assetTag} ORDER BY record_id DESC;`;
   }
