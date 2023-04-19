473394b9c899299d373411055873e3158f2b3ea7
jwehrman25 <poisondarttamer@gmail.com>
2023-04-19 13:36:50 -0500

    beets-256 better logic

--- client/src/components/EditUser/EditUser.js
+++ client/src/components/EditUser/EditUser.js
@@ -9,93 +9,37 @@ function EditUser(props) {
   const [user_id, setId] = useState(user.user_id);
   const [firstName, setFirstName] = useState(user.first_name);
   const [lastName, setLastName] = useState(user.last_name);
-  const [strikeOne, setStrikeOne] = useState(false);
-  const [strikeTwo, setStrikeTwo] = useState(false);
-  const [strikeThree, setStrikeThree] = useState(false);
   const [strikes, setStrikes] = useState(user.strikes);
 
   /**
-   * based of the user's incoming strike count sets the states for the strike buttons
-   */
-  function defaultStrikes() {
-    if (user.strikes > 2) {
-      //if the user has three or more strikes
-      setStrikeThree(true);
-      setStrikeTwo(true);
-      setStrikeOne(true);
-    } else if (user.strikes === 2) {
-      //if the user has two strikes
-      setStrikeTwo(true);
-      setStrikeOne(true);
-    } else if (user.strikes === 1) {
-      //if the user has one strike
-      setStrikeOne(true);
-    }
-    //since the strike states are set to false and 0, the default values for a user with zero strikes have already been assigned
-  }
-
-  /**
-   * logic for when the strike one button is pressed
+   * When button one is pressed sets strikes state to 1 if not 1, and sets strikes state to 0 if 1
    */
   function toggleStrikeOne() {
-    if (strikeOne) {
-      //if the first button is red
-      if (strikeThree) {
-        //if the third button is red set the third and second button to green and strikes to one
-        setStrikeThree(false);
-        setStrikeTwo(false);
-        setStrikeOne(true);
-        setStrikes(1);
-      } else if (strikeTwo) {
-        //if the second button is red set the second button to green and the strike to one
-        setStrikeTwo(false);
-        setStrikeOne(true);
-        setStrikes(1);
-      } else {
-        setStrikeOne(false); //set the button to green and strikes to 0
-        setStrikes(0);
-      }
+    if (strikes === 1) {
+      setStrikes(0);
     } else {
-      //if the button is green
-      setStrikeOne(true); //set strikes to 1 and button to red
       setStrikes(1);
     }
   }
 
+  /**
+   * When button two is pressed sets strikes state to 2 if not 2, and sets strikes state to 0 if 2
+   */
   function toggleStrikeTwo() {
-    if (strikeTwo) {
-      //if the second button is red
-      if (strikeThree) {
-        //if the third button is red set the third button to green and strikes to 2
-        setStrikeThree(false);
-        setStrikeTwo(true);
-        setStrikes(2);
-      } else {
-        //else set strikes to 0 and button two and one to green
-        setStrikeTwo(false);
-        setStrikeOne(false);
-        setStrikes(0);
-      }
+    if (strikes === 2) {
+      setStrikes(0);
     } else {
-      //if button is green set button two and one to red and strikes to 2
-      setStrikeTwo(true);
-      setStrikeOne(true);
       setStrikes(2);
     }
   }
 
+  /**
+   * When button one is pressed sets strikesstate  to 3 if not 3, and sets strikes state to 0 if 3 or greater
+   */
   function toggleStrikeThree() {
-    if (strikeThree) {
-      //if the button is red set all buttons to green and strikes to 0
-      setStrikeThree(false);
-      setStrikeTwo(false);
-      setStrikeOne(false);
+    if (strikes >= 3) {
       setStrikes(0);
     } else {
-      //if the button is green set all buttons to red and strikes to 3
-      setStrikeThree(true);
-      setStrikeTwo(true);
-      setStrikeOne(true);
       setStrikes(3);
     }
   }
@@ -163,10 +107,8 @@ function EditUser(props) {
     setAlertType(null);
   }
 
-  useEffect(() => {}, [strikeOne, strikeTwo, strikeThree]);
-  useEffect(() => {
-    defaultStrikes();
-  }, []);
+  //renders page when strikes changes
+  useEffect(() => {}, [strikes]);
 
   return (
     <Form>
@@ -215,7 +157,7 @@ function EditUser(props) {
       <Row>
         <Col>
           <Button
-            className={strikeOne ? "redButton" : "greenButton"}
+            className={strikes > 0 ? "redButton" : "greenButton"}
             onClick={() => {
               toggleStrikeOne();
             }}
@@ -223,7 +165,7 @@ function EditUser(props) {
         </Col>
         <Col>
           <Button
-            className={strikeTwo ? "redButton" : "greenButton"}
+            className={strikes > 1 ? "redButton" : "greenButton"}
             onClick={() => {
               toggleStrikeTwo();
             }}
@@ -231,13 +173,12 @@ function EditUser(props) {
         </Col>
         <Col>
           <Button
-            className={strikeThree ? "redButton" : "greenButton"}
+            className={strikes > 2 ? "redButton" : "greenButton"}
             onClick={() => {
               toggleStrikeThree();
             }}
           ></Button>
         </Col>
-        <Col></Col>
         <Col></Col>
         <Col></Col>
         <Col></Col>
